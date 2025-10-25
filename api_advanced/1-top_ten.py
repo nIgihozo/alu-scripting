7d183eea4c643b6a5be6f3437a8cc02166b6f5ef
nIgihozo <n.igihozo@gmail.com>
2025-10-25 19:23:59 +0000

    Updating queries again

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,38 +4,19 @@ import requests
 
 
 def top_ten(subreddit):
-    headers = {
-        'User-Agent': 'MyAPI/0.0.1',
-        'Accept': 'application/json' 
-    }
-    params = {'limit': 10}
-    
-    subreddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    
-    response = requests.get(
-        subreddit_url,
-        headers=headers,
-        params=params,
-        allow_redirects=False
-    )
 
-    if response.status_code != 200:
-        print(None)
-        return
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-    try:
+    if response.status_code == 200:
         json_data = response.json()
-        
-        posts = json_data.get('data', {}).get('children', [])
-        
-        if not posts:
-            print(None)
-            return
-
-        for post in posts:
-            title = post.get('data', {}).get('title')
-            if title:
-                print(title)
-
-    except (requests.exceptions.JSONDecodeError, AttributeError):
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
         print(None)
